801fa67ef4cb679017bc2c6ff811a64993cc064c
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-04-05 13:02:43 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,9 @@
 def slice_advanced():
     # Código a implementar utilizando input.
+    texto= "Awesome"
+    print(texto[0:3].lower())
+    print(texto[2:5])
+    print(texto.lower())
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
